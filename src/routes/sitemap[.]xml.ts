441cd875280e556cbf8e1db2fbd6ee845515db65
Pandit8787 <harshitmishra8006388244@gmail.com>
2026-08-18 02:20:31 +0530

    Update: latest changes

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -3,7 +3,7 @@ import type {} from "@tanstack/react-start";
 import { BLOGS } from "@/lib/site-data";
 
 // TODO: replace with your project URL once a project name or custom domain is set.
-const BASE_URL = "";
+const BASE_URL = "https://sportslifetennisacademy.com";
 
 interface SitemapEntry {
   path: string;
